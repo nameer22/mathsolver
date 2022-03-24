fa796bada3164f8244dae8dca9300b20fb3fa59f
nameeranjum <nameer.anjum@gmail.com>
2022-03-24 20:00:36 +0500

    Updated for python 3

--- mathsolver/__init__.py
+++ mathsolver/__init__.py
@@ -5,4 +5,4 @@ mathsolve is a library to evaluate and solve natural language mathematical equat
 __version__ = '0.1.1'
 __author__ = 'Vikram Singh'
 __email__ = 'mailvikram@email.com'
-__url__ = 'https://github.com/maverickjoy/mathsolver'
+__url__ = 'https://github.com/nameer22/mathsolver.git'
